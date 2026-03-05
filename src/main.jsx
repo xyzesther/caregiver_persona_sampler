76a78b9f94f4aeda2f620137dcc79da5bf63c886
xyzesther <zxyesther@gmail.com>
2026-03-05 14:14:40 -0800

    Updated main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import Profile from './Profile.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Profile />
+    <App />
   </StrictMode>
 );
